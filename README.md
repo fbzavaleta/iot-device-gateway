1e99d72d4a97c484d97d725ef99072e390a564ab
benjamin <benjamin1992.1992@gmail.com>
2022-09-07 19:39:23 -0300

    rtos files

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# iot-device-gateway
-A device gateway as micro-service for esp32 devices
+# Iot Device Gateway - IoT  Navigation System
+A [rtos]() navigation system based on HTTP request. Developed with my computer engineering students at the [FIAP]() university - São Paulo, Brasil.
+
+- [Setup](#local-setup)
+- [Development](#development)
+- [Contribution Guide](#contribution-guide)
+
+
+## Local Setup
+
+The hardware used id a robotic kit provided by [robocore](), and the sensor MPU6060:
+
+![explorer-kit](https://www.robocore.net/upload/tutoriais/345_header_H.jpg)
+
+The controller is an esp32 embedded on board VESPA developed by robocore for robotic projects, and the the follow diagram details the  pinout:
+
+![vespa-conections](https://s3-sa-east-1.amazonaws.com/robocore-lojavirtual/1439/vespa_esquematica-01.jpg)
+
+It is necesary to install the [esp-idf] on your computer, we recomend using the vscode plugin.
+
+## Contribution Guide
+
+You will need to create a new branch, follow this convention:
+
+```bash
+<turma>_<checkpoint>_<rm>
+```
+for example:
+
+```bash
+5ECR_Checkpoint2-A_rm3456
+```
+
+Create a Pull Request and add me to revise and aprove the features/changes.
+
+## Checkpoint2-A (2 ptos) - due 11/09 00:00
+
+At this time we have been created two task, at the collision task, we recibe the distance and print it like a decimal on meters only:
+
+![distance-example](https://drive.google.com/file/d/1bdrUqq8ZABwk_B0XuDA11Y6nwHBAHkGe/view?usp=sharing)
+
+we should storage meters and centimeters, please create a new structure to storage this distance values.
+
+
+
+
---
+++ robot-rtos/navigation-system/main/main.c
@@ -0,0 +1,141 @@
+/*
+ * Lib C
+ */
+#include <stdio.h>
+#include <stdint.h>  
+#include <string.h>
+
+/*
+ * FreeRTOS
+ */
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+/*
+ * Drivers
+ */
+#include "driver/gpio.h"
+#include "ultrasonic.h"
+
+/*
+ * logs
+ */
+#include "esp_log.h"
+
+/*
+ * definiçãos de GPIOs
+ */
+
+#define LED_GPIO    15  //Led_uso_geral D15
+#define ECHO_GPIO   26  //S1 D26
+#define TRIGG_GPIO  25  //S2 D25
+
+/*
+ * definições Gerais
+ */
+static const char * TAG = "MAIN-ROBOT: ";
+
+#define CMD_MEASURE	300
+#define MAX_DISTANCE_CM 500 // 5m max
+#define DEGUG 1
+
+/*
+ * Quees
+ */
+QueueHandle_t XQuee_ultrasonic;
+
+typedef struct {
+	uint16_t command;
+	uint32_t distance;
+	TaskHandle_t taskHandle;
+} CMD_t;
+
+
+
+void ultrasonic()
+{
+	CMD_t cmdBuf;
+	cmdBuf.command = CMD_MEASURE;
+	cmdBuf.taskHandle = xTaskGetCurrentTaskHandle();
+
+	ultrasonic_sensor_t sensor = {
+		.trigger_pin = TRIGG_GPIO,
+		.echo_pin = ECHO_GPIO
+	};
+
+	ultrasonic_init(&sensor);    
+
+	while (true) {
+		uint32_t distance;
+		esp_err_t res = ultrasonic_measure_cm(&sensor, MAX_DISTANCE_CM, &distance);
+		if (res != ESP_OK) {
+			printf("Error: ");
+			switch (res) {
+				case ESP_ERR_ULTRASONIC_PING:
+					printf("Cannot ping (device is in invalid state)\n");
+					break;
+				case ESP_ERR_ULTRASONIC_PING_TIMEOUT:
+					printf("Ping timeout (no device found)\n");
+					break;
+				case ESP_ERR_ULTRASONIC_ECHO_TIMEOUT:
+					printf("Echo timeout (i.e. distance too big)\n");
+					break;
+				default:
+					printf("%d\n", res);
+			}
+		} else {
+			ESP_LOGI(TAG,"Send Distance: %d cm, %.02f m\n", distance, distance / 100.0);
+			cmdBuf.distance = distance;
+			xQueueSend(XQuee_ultrasonic, &cmdBuf, portMAX_DELAY);
+		}
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}    
+}
+
+void collision()
+{
+	CMD_t cmdBuf;
+	uint8_t ascii[30];
+
+		for(;;)
+		{
+			xQueueReceive( XQuee_ultrasonic, &cmdBuf, portMAX_DELAY ); 	
+
+			strcpy((char*)ascii, "Ultrasonic DISTANCE");
+			sprintf((char*)ascii, "%d cm",cmdBuf.distance );
+			
+			
+			if( DEGUG ) 
+			{
+				ESP_LOGI(TAG,"\n\nDistance msg \ 
+									value: %s", ascii);
+			}
+
+			vTaskDelay( 10/portTICK_PERIOD_MS );	
+		}
+	vTaskDelete(NULL);
+}
+
+void app_main(void)
+{
+
+	if( (XQuee_ultrasonic = xQueueCreate( 10, sizeof(CMD_t)) ) == NULL )
+	{
+		ESP_LOGI( TAG, "error - nao foi possivel alocar XQuee_ultrasonic.\n" );
+		return;
+	} 
+
+    if( ( xTaskCreate( ultrasonic, "ultrasonic", 2048, NULL, 5, NULL )) != pdTRUE )
+	{
+		ESP_LOGI( TAG, "error - nao foi possivel alocar ultrasonic.\n" );	
+		return;		
+	}    
+
+    if( ( xTaskCreate( collision, "collision", 2048, NULL, 5, NULL )) != pdTRUE )
+	{
+		ESP_LOGI( TAG, "error - nao foi possivel alocar collision.\n" );	
+		return;		
+	}       
+
+}
