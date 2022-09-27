fe9aefb18cca52d44749bc905cef41e514ec7c79
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2022-09-27 14:32:29 -0300

    feat: checkpoint 2B

--- gateway/core/routes.py
+++ gateway/core/routes.py
@@ -1,10 +1,9 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 from gateway.core.enums.http_status import HttpStatus
 from gateway.core.response.types import ChannelFeed
 
-
-bp  =   Blueprint('core', __name__)
+bp      =   Blueprint('core', __name__)
 bp_v1   =   Blueprint('feed', __name__)
 bp_v2   =   Blueprint('status', __name__)
 
@@ -15,21 +14,23 @@ api_notes = {
     'device'        :   'esp32'
 }
 
-@bp.route('/',  methods=['GET'])
+@bp.route('/', methods=['GET'])
 def index():
-    return  jsonify(api_notes),   HttpStatus.OK.value
+    return jsonify(api_notes), HttpStatus.OK.value
 
-@bp.route('/conection-sensors',  methods=['POST'])
+
+@bp.route('/conection-sensors', methods=['POST'])
 def recibe_data():
-    return  jsonify(api_notes),   HttpStatus.OK.value    
+    print(request.form)
+    return jsonify(api_notes), HttpStatus.OK.value    
 
 
-@bp_v1.route('/',   methods=['GET'])
+@bp_v1.route('/', methods=['GET'])
 def index(): 
-    payload =   ChannelFeed({'device': 'esp32', 'data':{'fiel1':23}})
-    return  payload.to_response(),  HttpStatus.OK.value
+    payload = ChannelFeed({'device': 'esp32', 'data':{'fiel1':23}})
+    return payload.to_response(), HttpStatus.OK.value
 
 
-@bp_v2.route('/',   methods=['GET'])
+@bp_v2.route('/', methods=['GET'])
 def index(): 
     pass
