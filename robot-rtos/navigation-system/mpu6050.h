0aac53306bfc4160eabc343ceeb510ae2a1b771d
RodrigoKenji <60180438+DigoKenji@users.noreply.github.com>
2022-10-13 09:49:11 -0300

    Update mpu6050.h

--- robot-rtos/navigation-system/mpu6050.h
+++ robot-rtos/navigation-system/mpu6050.h
@@ -1,1 +1,14 @@
-//checkpoint2C- Adcicionar suporte
+extern void rgb_activate_M1();
+extern void Task_SendReceive ( void * pvParameter ) ;
+extern void Task_Socket ( void * pvParameter );
+extern void task_mpu6050();
+extern void init_mpu6050();
+void wifi_init_sta( void );
+
+
+typedef struct xData {
+ 	int sock; 
+ 	uint32_t x_accel; 
+	uint32_t y_accel; 
+	uint32_t z_accel; 	
+} xSocket_t;
