b88841386d7b3452e94c8baf66c5925c825ecdea
RodrigoKenji <60180438+DigoKenji@users.noreply.github.com>
2022-10-13 09:52:10 -0300

    Update qre1113.h

--- robot-rtos/navigation-system/qre1113.h
+++ robot-rtos/navigation-system/qre1113.h
@@ -1,3 +1,14 @@
+extern void rgb_activate_M1();
+extern void Task_SendReceive ( void * pvParameter ) ;
+extern void Task_Socket ( void * pvParameter );
+extern void task_mpu6050();
+extern void init_mpu6050();
+void wifi_init_sta( void );
 
-extern void anlogic_setup();
-extern uint32_t alalogic_read();
+
+typedef struct xData {
+ 	int sock; 
+ 	uint32_t x_accel; 
+	uint32_t y_accel; 
+	uint32_t z_accel; 	
+} xSocket_t;
