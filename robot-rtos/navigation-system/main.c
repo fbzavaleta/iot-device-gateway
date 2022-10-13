6dc48831e6c2506d55c5622955f5573c7f0e1413
KronusBoss <KronusBoss>
2022-10-13 20:55:08 -0300

    Adjustment main.c

--- robot-rtos/navigation-system/main.c
+++ robot-rtos/navigation-system/main.c
@@ -11,6 +11,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
+#include "freertos/event_groups.h"
 
 /*
  * Drivers
@@ -20,6 +21,7 @@
 #include "wifi_robot.h"
 #include "nvs_flash.h"
 #include "qre1113.h"
+#include "mpu6050.h"
 
 /*
  * logs
@@ -52,7 +54,10 @@ void http_SendReceive ( void * pvParameter );
 QueueHandle_t XQuee_ultrasonic;
 QueueHandle_t XQuee_comunications;
 QueueHandle_t XQuee_navigation;
+QueueHandle_t xQueue_timepout_x;
 
+
+static EventGroupHandle_t wifi_event_group;
 
 typedef struct {
 	uint16_t command;
@@ -189,12 +194,12 @@ void http_SendReceive(void * pvParameter)
         "POST /update HTTP/1.1\n"
         "Host: api.thingspeak.com\n"
         "Connection: close\n"
-        "X-THINGSPEAKAPIKEY: XNLVSMMPW8LO2M7I\n"
+        "X-THINGSPEAKAPIKEY: LS8M4UNZ2NYYYQIR\n"
         "Content-Type: application/x-www-form-urlencoded\n"
         "content-length: ";
 		
 	char databody[50];
-  	sprintf( databody, "{XNLVSMMPW8LO2M7I&field1=%d}", xSocket->distance);
+  	sprintf( databody, "{LS8M4UNZ2NYYYQIR&field1=%d}", xSocket->distance);
 	sprintf( buffer , "%s%d\r\n\r\n%s\r\n\r\n", msg_post, strlen(databody), databody);
 
   
@@ -258,13 +263,13 @@ checkpoint 2C - adicionar suporte para o chanel3
 
 void read_qre()
 {
-	uint32_t reflex_channel0;
+	adc1_struct reflex_channel0;
 
 	for (;;)
 	{
 		reflex_channel0 = alalogic_read();
 		xQueueSend(XQuee_navigation, &reflex_channel0, portMAX_DELAY);
-		ESP_LOGI(TAG, "Read analogic send to quee mV %d",reflex_channel0);
+		ESP_LOGI(TAG, "Read analogic send to quee mV %d and %d", reflex_channels.adc0_chars, reflex_channels.adc3_chars);
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
 	vTaskDelete(NULL);
@@ -295,8 +300,18 @@ void app_main(void)
 	wifi_config();
 	http_server_init();
 	anlogic_setup();
+
+	wifi_event_group = xEventGroupCreate();
+	wifi_init_sta();
+	init_mpu6050();
+
+	if ( (xQueue_timepout_x = xQueueCreate(25, sizeof(uint32_t))) ==NULL)
+	{
+         ESP_LOGI( TAG, "error - Nao foi possivel alocar a quee.\r\n" );  
+         return;  
+	}
 	
-	if( (XQuee_navigation = xQueueCreate( 10, sizeof(uint32_t)) ) == NULL )
+	if( (XQuee_navigation = xQueueCreate( 10, sizeof(adc1_struct)) ) == NULL )
 	{
 		ESP_LOGI( TAG, "error - nao foi possivel alocar XQuee_navigation.\n" );
 		return;
@@ -348,6 +363,18 @@ void app_main(void)
 	{
 		ESP_LOGI( TAG, "error - nao foi possivel alocar setup_sensor.\n" );	
 		return;		
-	}       
+	}    
+
+	if( xTaskCreate( Task_Socket, "task_socket", 4048, NULL, 5, NULL ) != pdTRUE )
+	{
+		ESP_LOGI( TAG, "error - nao foi possivel alocar Task_Socket.\n" );
+		return;
+	}
+
+	if( xTaskCreate( task_mpu6050, "task_mpu6050", 4048, NULL, 5, NULL ) != pdTRUE )
+	{
+		ESP_LOGI( TAG, "error - nao foi possivel alocar task_mpu6050.\n" );
+		return;
+	}   
 
 }
